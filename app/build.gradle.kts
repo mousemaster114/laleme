049407b5e35e989e9d003ecf679f0e38b8136cdc
mousemaster114 <mousemaster114@users.noreply.github.com>
2026-09-11 23:20:00 +0800

    v1.1.2：升级版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.laleme.app"
         minSdk = 31
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.1"
+        versionCode = 3
+        versionName = "1.1.2"
         vectorDrawables { useSupportLibrary = true }
     }
 
